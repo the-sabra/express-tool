e4c1dfefbc855439e8a343dc4e1dcbc9dce782d5
omars <omarsabra509@gmail.com>
2023-07-15 11:32:03 +0300

    for publish into pnpm and yarn

--- bin/ex-tool.js
+++ bin/ex-tool.js
@@ -2,7 +2,7 @@
 import { newAction } from "../actions/newAction.js";
 import { Command } from "commander";
 const program = new Command();
-program.name("et").version("1.0.4");
+program.name("et").version("1.0.5");
 
 program
   .command("new [project-name]")
