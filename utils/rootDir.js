d350350e6f7d81e951c071c5a98b2660ec89fc68
Kareem Mahlees <kareemmahlees@gmail.com>
2023-07-16 22:50:49 +0300

    update path resolve in rootDir, add eslint

--- utils/rootDir.js
+++ utils/rootDir.js
@@ -3,4 +3,4 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
-export default path.dirname(__filename);
+export default path.resolve(path.dirname(__filename), "..");
